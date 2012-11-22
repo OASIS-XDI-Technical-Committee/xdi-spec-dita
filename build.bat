adae14d8ee93535968a5fdc218d547b369d3afa7
peacekeeper <markus.sabadello@gmail.com>
2012-11-22 23:39:40 +0100

    setenv.bat and build.bat for windows

--- build.bat
+++ build.bat
@@ -1,31 +1,6 @@
 
-REM ** will convert sh to bat
-REM ** but it's not ready yet :)
-REM ** -Markus
-
-export DITA_DIR=`pwd`/DITA-OT1.5.4-ez
-
-if [ -f "$DITA_DIR"/tools/ant/bin/ant ] && [ ! -x "$DITA_DIR"/tools/ant/bin/ant ]; then
-chmod +x "$DITA_DIR"/tools/ant/bin/ant
-fi
-
-export ANT_OPTS="-Xmx512m $ANT_OPTS"
-export ANT_OPTS="$ANT_OPTS -Djavax.xml.transform.TransformerFactory=net.sf.saxon.TransformerFactoryImpl"
-export ANT_HOME="$DITA_DIR"/tools/ant
-export PATH="$DITA_DIR"/tools/ant/bin:"$PATH"
-
-NEW_CLASSPATH="$DITA_DIR/lib:$DITA_DIR/lib/dost.jar:$DITA_DIR/lib/commons-codec-1.4.jar:$DITA_DIR/lib/resolver.jar:$DITA_DIR/lib/icu4j.jar"
-NEW_CLASSPATH="$DITA_DIR/lib/saxon/saxon9.jar:$DITA_DIR/lib/saxon/saxon9-dom.jar:$NEW_CLASSPATH"
-if test -n "$CLASSPATH"
-then
-export CLASSPATH="$NEW_CLASSPATH":"$CLASSPATH"
-else
-export CLASSPATH="$NEW_CLASSPATH"
-fi
-
-
-java -jar DITA-OT1.5.4-ez/lib/dost.jar /i:$1 /transtype:pdf /ditadir:`pwd`/DITA-OT1.5.4-ez
-cp out/*.pdf docs/
-cp out/*.log logs/
-rm -rf out
-rm -rf temp
+java -jar DITA-OT1.5.4-ez\lib\dost.jar /i:%1 /transtype:pdf /ditadir:.\DITA-OT1.5.4-ez
+copy out\*.pdf docs\
+copy out\*.log logs\
+rmdir /s /q out
+rmdir /s /q temp
---
+++ setenv.bat
@@ -0,0 +1,23 @@
+
+SET DITA_DIR=.\DITA-OT1.5.4-ez
+
+IF DEFINED ANT_OPTS (
+  SET ANT_OPTS=-Xmx512m %ANT_OPTS%
+) ELSE (
+  SET ANT_OPTS=-Xmx512m
+)
+
+SET ANT_OPTS=%ANT_OPTS% -Djavax.xml.transform.TransformerFactory=net.sf.saxon.TransformerFactoryImpl
+SET ANT_HOME=%DITA_DIR%\tools\ant
+SET PATH=%DITA_DIR%\tools\ant\bin;%PATH%
+
+SET NEW_CLASSPATH=%DITA_DIR%\lib:%DITA_DIR%\lib\dost.jar:%DITA_DIR%\lib\commons-codec-1.4.jar:%DITA_DIR%\lib\resolver.jar:%DITA_DIR%\lib\icu4j.jar
+SET NEW_CLASSPATH=%DITA_DIR%\lib\saxon\saxon9.jar:%DITA_DIR%\lib\saxon\saxon9-dom.jar:%NEW_CLASSPATH%
+
+IF DEFINED CLASSPATH (
+  SET CLASSPATH=%NEW_CLASSPATH%:%CLASSPATH%
+) ELSE (
+  SET CLASSPATH=%NEW_CLASSPATH%
+)
+
+SET NEW_CLASSPATH=
