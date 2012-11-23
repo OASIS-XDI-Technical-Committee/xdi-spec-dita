748c0ef541985610f90381897e1993d68201196d
peacekeeper <markus.sabadello@gmail.com>
2012-11-23 14:20:18 +0100

    using %cd%

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
 
-java -jar DITA-OT1.5.4-ez\lib\dost.jar /i:%1 /transtype:pdf /ditadir:.\DITA-OT1.5.4-ez
+java -jar DITA-OT1.5.4-ez\lib\dost.jar /i:%1 /transtype:pdf /ditadir:%CD%\DITA-OT1.5.4-ez
 copy out\*.pdf docs\
 copy out\*.log logs\
 rmdir /s /q out
